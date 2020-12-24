a5b3ad3a32d4966565ff2b0c067a57d5a094fd36
Oleg Tielushko <tielushko@mail.usf.edu>
2020-12-24 10:18:47 -0500

    Start

---
+++ android/app/src/main/kotlin/oleg/tielushko/flutter_business_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package oleg.tielushko.flutter_business_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('Images/Avatar.jpeg'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
